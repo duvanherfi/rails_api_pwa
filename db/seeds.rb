958e97a3388a84c83d3bc919684f7d6ce88ac1e7
Duvan Hernandez <dhernandez@pibox.app>
2024-03-16 13:27:40 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,14 @@ Type.where(name: "Natural").first_or_create
 Type.where(name: "Juridica").first_or_create
 
 Position.where(name: "Administrador").first_or_create
+Position.where(name: "Obrero").first_or_create
+Position.where(name: "Arquitecto").first_or_create
+Position.where(name: "Super user").first_or_create
+
+User.where(email: "admin@admin.com").first_or_create do |user|
+  user.name = "Admin"
+  user.password = "admin"
+  user.phone = "1234567890"
+  user.identification = "1234567890"
+  user.position = Position.where(name: "Super user").first.id
+end
