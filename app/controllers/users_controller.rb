7d3b8a31bbbdb1ee10590ce2a0dab77e25e95fd1
Duvan Hernandez <dhernandez@pibox.app>
2024-03-18 18:55:19 -0500

    comment token validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show update destroy ]
-  before_action :validate_session_token, except: [:login, :create]
+  #before_action :validate_session_token, except: [:login, :create]
 
   # GET /users
   def index
