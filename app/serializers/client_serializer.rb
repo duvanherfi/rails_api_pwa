f80a046d2fc1745b93948855d9d23edb454d7823
Duvan Hernandez <dhernandez@pibox.app>
2024-03-18 19:14:49 -0500

    fix

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,5 +1,5 @@
 class ClientSerializer < ActiveModel::Serializer
-  attributes :_id, :name, :email, :phone, :identification, :created_at, :updated_at, :session_token
+  attributes :_id, :name, :email, :phone, :identification, :created_at, :updated_at
   belongs_to :type
 
   def _id
